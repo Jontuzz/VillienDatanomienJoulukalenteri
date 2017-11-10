370e714083328e71a4c8c830e25d2619d4ae1162
Joonas <joonas.s.jh@gmail.com>
2017-11-10 12:34:09 +0200

    lisätty tiedoston luku metodi

--- VillienDatanomienJoulukalenteri/src/tiedostonkasittelypakkaus/Tiedostonkasittelija.java
+++ VillienDatanomienJoulukalenteri/src/tiedostonkasittelypakkaus/Tiedostonkasittelija.java
@@ -5,10 +5,26 @@
  */
 package tiedostonkasittelypakkaus;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 /**
  *
  * @author s1601378
  */
 public class Tiedostonkasittelija {
+    
+    public ArrayList<String> lueLuukkujenSisallot() {
+        ArrayList<String> rivit = new ArrayList<>();
+        try {
+            Files.lines(Paths.get("luukkujenSisallot.txt")).forEach(rivi -> rivit.add(rivi));
+        } catch (IOException e) {
+            System.out.println("Lukeminen epäonnistui. Virhe: " + e.getMessage());
+        }
+
+        return rivit;
+    }
     
 }
