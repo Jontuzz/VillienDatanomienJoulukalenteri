b7786f176a5bc03954c5f2eab9bec20c289a98e8
Joonas <joonas.s.jh@gmail.com>
2017-11-10 09:50:02 +0200

    nappeja päivitetty

--- VillienDatanomienJoulukalenteri/src/villiendatanomienjoulukalenteri/VillienDatanomienJoulukalenteriController.java
+++ VillienDatanomienJoulukalenteri/src/villiendatanomienjoulukalenteri/VillienDatanomienJoulukalenteriController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -20,6 +21,78 @@ public class VillienDatanomienJoulukalenteriController implements Initializable 
     
     @FXML
     private Label label;
+    
+    @FXML
+    private Button luukku1;
+    
+    @FXML
+    private Button luukku2;
+    
+    @FXML
+    private Button luukku3;
+    
+    @FXML
+    private Button luukku4;
+    
+    @FXML
+    private Button luukku5;
+    
+    @FXML
+    private Button luukku6;
+    
+    @FXML
+    private Button luukku7;
+    
+    @FXML
+    private Button luukku8;
+    
+    @FXML
+    private Button luukku9;
+    
+    @FXML
+    private Button luukku10;
+    
+    @FXML
+    private Button luukku11;
+    
+    @FXML
+    private Button luukku12;
+    
+    @FXML
+    private Button luukku13;
+    
+    @FXML
+    private Button luukku14;
+    
+    @FXML
+    private Button luukku15;
+   
+    @FXML
+    private Button luukku16;
+            
+    @FXML
+    private Button luukku17;
+    
+    @FXML
+    private Button luukku18;
+    
+    @FXML
+    private Button luukku19;
+    
+    @FXML
+    private Button luukku20;
+    
+    @FXML
+    private Button luukku21;
+    
+    @FXML
+    private Button luukku22;
+    
+    @FXML
+    private Button luukku23;
+    
+    @FXML
+    private Button luukku24;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
