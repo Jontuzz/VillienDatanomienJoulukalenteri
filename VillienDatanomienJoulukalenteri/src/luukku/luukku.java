4ae763e7add03e7953a4fa1abc7833be861abb31
Hiltunen Joonas <s1601378@student.businesscollege.fi>
2017-11-12 14:13:28 +0200

    lisätty luukku.java

---
+++ VillienDatanomienJoulukalenteri/src/luukku/luukku.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luukku;
+
+/**
+ *
+ * @author s1601378
+ */
+public class luukku {
+    
+    
+    
+}
